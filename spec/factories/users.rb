b25c9b7a8122026cb6b87194f68460e43b4fff96
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-01-17 17:25:17 +0900

    ユーザー新規登録のモデルスペックテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :user do
+    user_name { "侍君一号" }
+    sequence(:email) { |n| "user_#{n}@example.com" }
+    password { "katanagari" }
+    password_confirmation { "katanagari" }
   end
 end
