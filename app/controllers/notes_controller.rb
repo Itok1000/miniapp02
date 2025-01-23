39bfc92c72ed5b95a1d00765cba19ba87e2c481a
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-01-23 18:40:33 +0900

    試合記録試合記録一覧の実装

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,45 @@
 class NotesController < ApplicationController
+    before_action :set_note, only[:edit, :update, :destroy]
+    def index
+        @notes = Note.includes(:user)
+    end
+
+    def new
+        @note = Note.new
+    end
+
+    def create
+        @notes = Note.new(note_params)
+        if @note.save
+            redirect to notes_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @note.update(note_params)
+            redirect to notes_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @note.destroy(note_params)
+        redirect to notes_path
+    end
+
+    private
+
+    def note_params
+        params.require(:post).permit(:title, :content)
+    end
+
+    def set_note
+        @note = Note.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
-  resources :notes, only: %i[:index,:new,:destory]
+  resources :notes, only: %i[index new create destroy edit update]
 end
 
 # ●resources について
