649c19b6d6c6527d34eac9d41004eb2c306d46b6
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-01-23 15:42:42 +0900

    「このアプリの使い方」の初期設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
-    skip_before_action :require_login, only: %i[top]
+    skip_before_action :require_login, only: %i[top how_to_use]
 
     def top; end
+
+    def how_to_use; end
 end
 
 # ●skip_before_action について
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "static_pages#top"
+  get "/how_to_use", to: "static_pages#how_to_use"
+  
   resources :users, only: %i[new create]
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
