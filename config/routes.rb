056590e811a0bf0699739f161097712dc9fbaf5b
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-01-23 16:51:37 +0900

    試合記録試合記録一覧の実装

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
-  resources :boards, only: %i[:index,:new,:destory]
+  resources :notes, only: %i[:index,:new,:destory]
 end
 
 # ●resources について
