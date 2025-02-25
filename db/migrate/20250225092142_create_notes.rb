896b00e2c0e3332c62b0d5894541b73fcddf1869
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-02-25 19:08:00 +0900

    試合記録一覧表の作成(モデルの実装とマイグレ作成)

--- db/migrate/20250225092142_create_notes.rb
+++
@@ -1,9 +0,0 @@
-class CreateNotes < ActiveRecord::Migration[7.2]
-  create_table :boards do |t|
-    t.string :title, null: false
-    t.text   :content, null: false
-    t.references :user, foreign_key: true
-
-    t.timestamps
-  end
-end
