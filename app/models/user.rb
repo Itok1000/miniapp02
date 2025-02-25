f639ef0a5fa5dbfcd37cf7abdaf8a59bf62bef6c
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-02-25 18:31:37 +0900

    試合記録一覧表の作成(モデルの実装とマイグレ作成)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :user_name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
+
+  has_many :notes, dependent: :destroy
 end
 
 # 上記のコードには以下のバリデーションが設けられている
